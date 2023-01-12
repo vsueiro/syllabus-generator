927bf226ab9755ebe15645d897258559d0277ef0
Vinicius Sueiro <hi@vsueiro.com>
2023-01-12 00:20:01 -0500

    Start library

---
+++ js/timeline.js
@@ -0,0 +1,37 @@
+(function () {
+  // Find all timeline placeholders
+  let timelineElements = document.querySelectorAll(".timeline");
+
+  // For each timeline
+  for (let timelineElement of timelineElements) {
+    // Get Google Spreadsheet details
+    const sheet = "";
+    const range = "";
+
+    // Load data
+
+    console.log(timelineElement);
+  }
+})();
+
+/*
+<div class="legend">
+  <div>
+    <div class="block" data-class="Yes"></div>
+    <span>Class day</span>
+  </div>
+  <div>
+    <div class="block" data-class="No"></div>
+    <span>No class</span>
+  </div>
+  <div>
+    <div class="block" data-event="Presentation"></div>
+    <span>Presentation day</span>
+  </div>
+  <div>
+    <div class="block" data-event="Final Exam"></div>
+    <span>Final exam day</span>
+  </div>
+</div>
+<div class="chart rows"></div>
+*/
